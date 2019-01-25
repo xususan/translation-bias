3165ce02c1812a63cbdef6caf2a1d133b6c4e850
xususan <susan.xu97@gmail.com>
2019-01-24 23:03:05 -0500

    rename files

--- process_data.py
+++ process_data.py
@@ -2,8 +2,8 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-PATH_TO_TR = "../opus/bitext.tr"
-PATH_TO_EN = "../opus/bitext.en"
+PATH_TO_TR = "../opus/Open-Subtitles.en-tr.tr"
+PATH_TO_EN = "../opus/Open-Subtitles.en-tr.en"
 
 subs_en = open(PATH_TO_EN, encoding='utf-8').read().split('\n')
 subs_tr = open(PATH_TO_TR, encoding='utf-8').read().split('\n')
