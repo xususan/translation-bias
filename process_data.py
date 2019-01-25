62119a92b5505ff59aee9c1acba64ba12c4b21cf
xususan <susan.xu97@gmail.com>
2019-01-25 18:01:53 -0500

    before changing src / trg on data fields

--- process_data.py
+++ process_data.py
@@ -1,14 +1,33 @@
 import random
-import pdb
-#import torchtext
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import argparse
+import logging
 
-PATH_TO_TR = "../opus/OpenSubtitles.en-tr.tr"
-PATH_TO_EN = "../opus/OpenSubtitles.en-tr.en"
+# Set up parser for arguments
+parser = argparse.ArgumentParser(description='Data Processing')
+parser.add_argument('--size', type=str, default="full", help='Size of file (full or mini)')
+args = parser.parse_args()
 
-# PATH_TO_TR = "../opus/opensubs.tr.mini"
-# PATH_TO_EN = "../opus/opensubs.en.mini"
+print(f"Processing {args.size} size data...")
+if args.size == "full":
+    PATH_TO_TR = "data/OpenSubtitles.en-tr.tr"
+    PATH_TO_EN = "data/OpenSubtitles.en-tr.en"
+    # create train and validation set
+    train_size = int(2E6)
+    val_size = 10000
+    test_size = 10000
+    print(f"training size: {train_size}, validation size: {val_size}")
+    train_path, val_path, test_path = "data/train_2m.csv", "data/val_10k.csv", "data/test_10k.csv"
+elif args.size == "mini":
+    PATH_TO_TR = "data/opensubs.tr.mini"
+    PATH_TO_EN = "data/opensubs.en.mini"
+    # create train and validation set
+    train_size = 1000
+    val_size = 100
+    test_size = 100
+    print(f"training size: {train_size}, validation size: {val_size}")
+    train_path, val_path, test_path = "data/train_mini.csv", "data/val_mini.csv", "data/test_mini.csv"
 
 subs_en = open(PATH_TO_EN, encoding='utf-8').read().split('\n')[:-1]
 subs_tr = open(PATH_TO_TR, encoding='utf-8').read().split('\n')[:-1]
@@ -23,21 +42,18 @@ df = df.applymap(remove_dashes)
 # Remove very long sentences
 df['eng_len'] = df['en'].str.count(' ')
 df['tr_len'] = df['tr'].str.count(' ')
-df = df.query('tr_len < d80 & eng_len < 80')
+# df = df.query('tr_len < d80 & eng_len < 80')
 
 # Drop eng_len, tr_len columns
 df = df[['tr','en']]
 
-# create train and validation set
-train_size = 2E6
-val_size = 10000
-test_size = 10000
-print(f"training size: {train_size}, validation size: {val_size}")
-
 # Select correct number of indices
 val_indices = random.sample(range(1, df.shape[0]), val_size)
-train_indices = list(set(range(1, df.shape[0])) - set(val_indices))
-test_indices = list(set(range(1, df.shape[0])) - set(val_indices) - set(train_indices))
+test_indices = random.sample(set(range(1, df.shape[0])) - set(val_indices), test_size)
+train_indices = random.sample(set(range(1, df.shape[0])) - set(val_indices) - set(test_indices), train_size)
+assert(len(val_indices) == val_size)
+assert(len(test_indices) == test_size)
+assert(len(train_indices) == train_size)
 
 def select_indices(indices, df, file_path):
     val_df = df.iloc[indices]
@@ -51,7 +67,7 @@ def select_indices(indices, df, file_path):
     print(f"Wrote to {file_path}")
     return
 
-select_indices(val_indices, df, "../opus/val_10k.csv")
-select_indices(test_indices, df, "../opus/test_10k.csv")
-select_indices(train_indices, df, "../opus/train_2m.csv")
+select_indices(val_indices, df, val_path)
+select_indices(test_indices, df, test_path)
+select_indices(train_indices, df, train_path)
 
---
+++ utils_transform.py
@@ -0,0 +1,131 @@
+import time
+import torch
+import numpy as np
+from transformer import subsequent_mask
+from torchtext import data
+import pdb
+class Batch:
+    "Object for holding a batch of data with mask during training."
+    def __init__(self, src, trg=None, pad=0):
+        self.src = src
+        self.src_mask = (src != pad).unsqueeze(-2)
+        if trg is not None:
+            self.trg = trg[:, :-1]
+            self.trg_y = trg[:, 1:]
+            self.trg_mask = \
+                self.make_std_mask(self.trg, pad)
+            self.ntokens = (self.trg_y != pad).data.sum()
+    
+    @staticmethod
+    def make_std_mask(tgt, pad):
+        "Create a mask to hide padding and future words."
+        tgt_mask = (tgt != pad).unsqueeze(-2)
+        tgt_mask = tgt_mask & (
+            subsequent_mask(tgt.size(-1)).type_as(tgt_mask.data))
+        return tgt_mask
+
+def run_epoch(data_iter, model, loss_compute):
+    "Standard Training and Logging Function"
+    start = time.time()
+    total_tokens = 0.0
+    total_loss = 0
+    tokens = 0.0
+    for i, batch in enumerate(data_iter):
+        out = model.forward(batch.src, batch.trg, 
+                            batch.src_mask, batch.trg_mask)
+        batch_ntokens = batch.ntokens.float()
+        loss = loss_compute(out, batch.trg_y, batch_ntokens)
+        total_loss += loss
+        total_tokens += batch_ntokens
+        tokens += batch_ntokens
+        if i % 50 == 1:
+            elapsed = time.time() - start
+            print("Epoch Step: %d Loss: %f Tokens per Sec: %f" %
+                    (i, loss / batch_ntokens, tokens / elapsed))
+            start = time.time()
+            tokens = 0.0
+    return total_loss / total_tokens
+
+global max_src_in_batch, max_tgt_in_batch
+def batch_size_fn(new, count, sofar):
+    "Keep augmenting batch and calculate total number of tokens + padding."
+    global max_src_in_batch, max_tgt_in_batch
+    if count == 1:
+        max_src_in_batch = 0
+        max_tgt_in_batch = 0
+    max_src_in_batch = max(max_src_in_batch,  len(new.src))
+    max_tgt_in_batch = max(max_tgt_in_batch,  len(new.trg) + 2)
+    src_elements = count * max_src_in_batch
+    tgt_elements = count * max_tgt_in_batch
+    return max(src_elements, tgt_elements)
+
+class NoamOpt:
+    "Optim wrapper that implements rate."
+    def __init__(self, model_size, factor, warmup, optimizer):
+        self.optimizer = optimizer
+        self._step = 0
+        self.warmup = warmup
+        self.factor = factor
+        self.model_size = model_size
+        self._rate = 0
+        
+    def step(self):
+        "Update parameters and rate"
+        self._step += 1
+        rate = self.rate()
+        for p in self.optimizer.param_groups:
+            p['lr'] = rate
+        self._rate = rate
+        self.optimizer.step()
+        
+    def rate(self, step = None):
+        "Implement `lrate` above"
+        if step is None:
+            step = self._step
+        return self.factor * \
+            (self.model_size ** (-0.5) *
+            min(step ** (-0.5), step * self.warmup ** (-1.5)))
+        
+def get_std_opt(model):
+    return NoamOpt(model.src_embed[0].d_model, 2, 4000,
+            torch.optim.Adam(model.parameters(), lr=0, betas=(0.9, 0.98), eps=1e-9))
+
+class SimpleLossCompute:
+    "A simple loss compute and train function."
+    def __init__(self, generator, criterion, opt=None):
+        self.generator = generator
+        self.criterion = criterion
+        self.opt = opt
+        
+    def __call__(self, x, y, norm):
+        x = self.generator(x)
+        loss = self.criterion((x.contiguous().view(-1, x.size(-1))), 
+                              (y.float().contiguous().view(-1))) / norm
+        loss.backward()
+        if self.opt is not None:
+            self.opt.step()
+            self.opt.optimizer.zero_grad()
+        return loss.data.item() * norm
+
+class MyIterator(data.Iterator):
+    def create_batches(self):
+        if self.train:
+            def pool(d, random_shuffler):
+                for p in data.batch(d, self.batch_size * 100):
+                    p_batch = data.batch(
+                        sorted(p, key=self.sort_key),
+                        self.batch_size, self.batch_size_fn)
+                    for b in random_shuffler(list(p_batch)):
+                        yield b
+            self.batches = pool(self.data(), self.random_shuffler)
+            
+        else:
+            self.batches = []
+            for b in data.batch(self.data(), self.batch_size,
+                                          self.batch_size_fn):
+                self.batches.append(sorted(b, key=self.sort_key))
+
+def rebatch(pad_idx, batch):
+    "Fix order in torchtext to match ours"
+    src, trg = batch.src.transpose(0, 1), batch.trg.transpose(0, 1)
+    return Batch(src, trg, pad_idx)
