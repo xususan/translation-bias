1e5f23111517482915aef027530fd23314f22c3c
xususan <susan.xu97@gmail.com>
2019-02-13 11:53:02 -0500

    fixed nonetype issue

--- eval.py
+++ eval.py
@@ -7,6 +7,21 @@ import spacy
 import pdb
 import argparse
 
+"""
+Use this script to evaluate the results of a trained model (checkpoint). 
+
+The architecture of the model (transformer.py) MUST be the same as training.
+
+ONLY COMPATIBLE WITH MODELS TRAINED AFTER 2/11.
+
+Notes on vocab: the SRC language MUST have two additional tokens (unk / pad), 
+TRG language MUST have four additional tokens (EOS / BOS / unk / pad)
+
+Also, you must build the vocab in the exact same way it was built during training
+to ensure that you have the same tokenizer. At least for now, which is kinda sus.
+
+CPU ONLY.
+"""
 # Set up parser for arguments
 parser = argparse.ArgumentParser(description='Evaluating performance of a model')
 parser.add_argument('--vocab', type=int, default=10000, help='Vocab size. MUST MATCH')
@@ -22,13 +37,7 @@ BATCH_SIZE = args.batch
 train_path = "train_200k.csv" # TRAIN PATH MUST MATCH ORIGINAL
 SOS, EOS, PAD, BOS = "<s>", "</s>", "<pad>", "<bos>" # Represents begining of context sentence
 
-if torch.cuda.device_count() > 0:
-  print('GPUs available:', torch.cuda.device_count())
-  model.cuda()
-  criterion.cuda()
-  device = torch.device('cuda', 0)
-else:
-  device = torch.device('cpu')
+device = torch.device('cpu')
 
 
 def rebatch_for_eval(pad_idx, batch):
@@ -42,6 +51,8 @@ def rebatch_for_eval(pad_idx, batch):
   return Batch(src, trg_correct, src_context, pad_idx), Batch(src, trg_incorrect, src_context, pad_idx)
 
 def log_likelihood(model, batch):
+    """Calculates the log likelihood of a batch, given the model.
+    """
     memory = model.encode(batch)
     total_prob = torch.zeros(batch.trg_y.size(0))
     for i in range(0, batch.trg_y.size(1)): # trg_len
@@ -57,6 +68,8 @@ def log_likelihood(model, batch):
     return total_prob
 
 def load(path):
+    """Loads a trained model from memory for evaluation.
+    """
     tr_voc = VOCAB_SIZE + 2
     en_voc = VOCAB_SIZE + 4
     model = make_model(tr_voc, en_voc, N=6)
@@ -83,7 +96,8 @@ def greedy_decode(model, batch, max_len, start_symbol):
     return ys
 
 def beam_decode(model, src, src_mask, src_context, pad_idx, max_len, start_symbol, end_symbol, k=5):
-    pdb.set_trace()
+    """Beam decoder.
+    """
     batch = Batch(src.unsqueeze(0), src_mask.unsqueeze(0), src_context.unsqueeze(0), pad_idx)
     memory = model.encode(batch)
     ys = torch.ones(1,1).fill_(start_symbol).type_as(src.data)
@@ -106,26 +120,32 @@ def beam_decode(model, src, src_mask, src_context, pad_idx, max_len, start_symbo
           new_probs = top_probs.flatten().data + previous_prob
           candidates_at_length = candidates_at_length + list(zip(new_hypotheses, new_probs))
       hypotheses = sorted(candidates_at_length, key = lambda x: x[1], reverse=True)[:k]
-    return hypotheses[0]
+    return hypotheses[0][0].squeeze() # change to 1D tensor
 
-lambda str_of_tokens: [EN.vocab.itos[i] for i in tokenized]
-
-def eval_bleu(pad_idx, eval_iter, model, max_len, start_symbol, end_symbol):
+def eval_bleu(pad_idx, eval_iter, model, max_len, start_symbol, end_symbol, rev_tokenize_trg):
+  """Calculates average BLEU score of a model on some validation iterator.
+  """
   bleus = []
-  for b in eval_iter:
-    b = rebatch(pad_idx, b)
-    for i in range(b.src.size(0)): # batch_size
-      hypothesis = beam_decode(model, b.src[i], b.src_mask[i], b.src_context[i],
+  for old_batch in eval_iter:
+    batch = rebatch(pad_idx, old_batch)
+    for i in range(batch.src.size(0)): # batch_size
+      hypothesis = beam_decode(model, batch.src[i], batch.src_mask[i], batch.src_context[i],
        pad_idx, max_len, start_symbol, end_symbol, k=5)[1:] # cut off SOS
-      targets = b.trg_y # doesn't have SOS
-      hyp_str = str_of_tokens(hypothesis).join(" ")
-      trg_str = str_of_tokens(targets).join(" ")
-      pdb.set_trace()
+      targets = batch.trg_y[i] # doesn't have SOS
+      hyp_str = " ".join(rev_tokenize_trg(hypothesis))
+      trg_str = " ".join(rev_tokenize_trg(targets))
       bleu = get_moses_multi_bleu([hyp_str], [trg_str])
-      bleus.append(bleu)
+
+      if bleu == None: # Error
+        print(i, hyp_str, trg_str)
+      else:
+        bleus.append(bleu)
   return sum(bleus) / len(bleus)
 
-def eval_accuracy(pad_idx, eval_iter, model):
+def eval_accuracy_helper(pad_idx, eval_iter, model):
+  """ Helper function that calculates how well the model chooses the correct
+  pronoun on the data in an iterator.
+  """
   n_correct = 0.0
   n_total = 0.0
   for b in eval_iter:
@@ -139,7 +159,10 @@ def eval_accuracy(pad_idx, eval_iter, model):
   print("Correct: %d / %d = %f" % (n_correct, n_total, (n_correct / n_total)))
   return
 
-def eval_discriminative(pad_idx, path_to_test_set, model):
+def eval_accuracy(pad_idx, path_to_test_set, model):
+  """Calculates how well the model chooses the correct pronoun, given
+  a dataset in TSV form. 
+  """
   full_path = "data/%s" % (path_to_test_set)
   print('Evaluating discriminative dataset: %s ' % (full_path))
   test = TabularDataset(
@@ -150,8 +173,7 @@ def eval_discriminative(pad_idx, path_to_test_set, model):
   test_iter = Iterator(
     test, batch_size=100, sort_key=lambda x: 1, repeat=False, train=False)
 
-  eval_discrim(pad_idx, test_iter, model)
-
+  eval_accuracy_helper(pad_idx, test_iter, model)
 
 
 print('loading spacy')
@@ -161,9 +183,10 @@ def tokenize_en(sentence):
     return [tok.text for tok in en.tokenizer(sentence)]
 
 # Context and source / target fields for English + Turkish
-# TODO:CHECK THIS ## NEED TO FLIP ONCE U GET NEW MODEL
-TR = Field(init_token = SOS, eos_token = EOS, lower = True, pad_token=PAD) 
-EN = Field(tokenize=tokenize_en, lower=True, pad_token=PAD)
+TR = Field(lower=True, pad_token=PAD)
+EN = Field(tokenize=tokenize_en, lower=True, pad_token=PAD, init_token = SOS, eos_token =EOS)
+rev_tokenize_en = lambda tokenized: [EN.vocab.itos[i] for i in tokenized]
+rev_tokenize_tr = lambda tokenized: [TR.vocab.itos[i] for i in tokenized]
 
 # Must be in order
 # !!! IMPT note there are five fields
@@ -203,7 +226,7 @@ print("Model loaded.")
 
 if args.eval == "accuracy" or args.eval == "all":
   for path in ["pro_stereotype.tsv", "anti_stereotype.tsv", "male_subject.tsv", "female_subject.tsv"]:
-    eval_discriminative(pad_idx, path, model)
+    eval_accuracy(pad_idx, path, model)
 
 if args.eval == "bleu" or args.eval == "all":
-  eval_bleu(pad_idx, valid_iter, model, 10, EN.vocab.stoi[SOS],EN.vocab.stoi[EOS])
+  eval_bleu(pad_idx, valid_iter, model, 10, EN.vocab.stoi[SOS],EN.vocab.stoi[EOS], rev_tokenize_en)
