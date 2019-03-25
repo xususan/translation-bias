8a08e45ea1996e6f3abe8407f99912aef7ed6ccf
xususan <susan.xu97@gmail.com>
2019-03-25 18:28:42 -0400

    forgot to pass multigpu arg

--- utils_transform.py
+++ utils_transform.py
@@ -41,7 +41,7 @@ def run_epoch(data_iter, model, loss_compute, multi_gpu):
     total_loss = 0
     tokens = 0.0
     for i, batch in enumerate(data_iter):
-        out = model.forward(batch)
+        out = model.forward(batch, multi_gpu=multi_gpu)
         batch_ntokens = batch.ntokens.float()
         loss = loss_compute(out, batch.trg_y, batch_ntokens)
         total_loss += loss
